d427e3f8170166a57b76df82c75360ebcef16872
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-01-05 01:57:33 -0500

    Update Get-ComputerInventory

--- Event 0 - Practice/Get-ComputerInventory.ps1
+++ Event 0 - Practice/Get-ComputerInventory.ps1
@@ -1,67 +1,80 @@
 ﻿<#
 # TO DO
 -Should OS and SP being imported from the list of IP ?
--What if multiple diskdrive
--Should i get the OS and SP info
--LastReboot information : is the property LastBootUpTime good enough ?
--order the final $info
 -Validate Parameter
 -Files: Validate Naming Convention accepted by Windows ([A-Z]|[a-z]|[0-9]|_|-|\.|\s)+
--Some change
--if WSMAN fail, fall back on DCOM
+-if WSMAN (v3) fail, fall back on DCOM, fallback on pssession (wsman v2)
 -LogPath parameter ?
 -Save in CSV format, one file per computer COMPUTERNAME_Inventory_YYYYMMDD-HHMMSS.csv
+-lastbootuptime does not seem accurate
+-Job ? workflow (parallele work)?
+-File format should include ? LR LP HW APP CMPNT
+-win32_product
+
+-localdisk: Where-Object {$_.InterfaceType -like "IDE"}	|
 #>
 
 
 function Get-ComputerInventory {
 <#
 	.SYNOPSIS
-		Get-ComputerInformation function retrieve inventory information from one or multiple computers.
+		Get-ComputerInventory function retrieve inventory information from one or multiple computers.
 
 	.DESCRIPTION
-		Get-ComputerInformation function retrieve inventory information from one or multiple computers.
+		Get-ComputerInventory function retrieve inventory information from one or multiple computers.
 
 	.PARAMETER  ComputerName
 		Specifies Defines the ComputerName
 
 	.PARAMETER  Path
-		Specifies different credential to use
+		Specifies the Path where to export the data from each computer
+		The default filename used by the script is: Inventory-<COMPUTERNAME>-yyyyMMdd_hhmmss.xml
 	
 	.PARAMETER  Protocol
 		Specifies the protocol to use to establish the connection with the remote computer(s)
-		Default: WSMAN
+		If not specified the script will try first with WSMAN then with DCOM
 	
 	.PARAMETER  HardwareInformation
-		Specifies different credential to use
+		Gather information related to the Hardware
 	
 	.PARAMETER  LastPatchInstalled
-		Specifies different credential to use
+		Gather information on the last patch installed
 	
 	.PARAMETER  LastReboot
-		Specifies different credential to use
+		Gather information of the last reboot
 	
-	.PARAMETER  ApplicationInstalled
-		Specifies different credential to use
+	.PARAMETER  ApplicationsInstalled
+		Verify if IIS, SQL, Sharepoint or Exchange is installed
 	
 	.PARAMETER  WindowsComponents
-		Specifies different credential to use
+		Gather the Windows Features installed on the computer
 
 	.EXAMPLE
-		PS C:\> Get-Computer -ParameterA 'One value' -ParameterB 32
-		'This is the output'
-		This example shows how to call the Get-Something function with named parameters.
-
+		Get-ComputerInventory -ComputerName LOCALHOST
+	
+		ComputerName                               Connectivity
+		------------                               ------------
+		LOCALHOST                                  Online
+	
+		This example shows what return the cmdlet using only the ComputerName parameter.
+	
 	.EXAMPLE
-		PS C:\> Get-Something 'One value' 32
-		'This is the output'
-		This example shows how to call the Get-Something function with positional parameters.
+		Get-ComputerInventory -ComputerName SERVER01 -HardwareInformation
+	
+		Manufacturer       : System manufacturer
+		LocalDisks         : @{DeviceID=\\.\PHYSICALDRIVE0; SizeGB=111.79}
+		ComputerName       : SERVER01
+		MemoryGB           : 4.00
+		NumberOfProcessors : 1
+		Model              : System Product Name
+		Connectivity       : Online
+	
+		This example shows what return the cmdlet using the switch HardwareInformation.
 
-	.INPUTS
-		System.String,System.Int32
 
 	.OUTPUTS
-		System.String
+		PsCustomObject
+		CliXML file
 
 	.NOTES
 		Winter Scripting Games 2014
@@ -74,7 +87,7 @@ function Get-ComputerInventory {
 
 	[CmdletBinding()]
 	PARAM(
-		[Alias("MachineName","__SERVER")]
+		[Alias("__SERVER","PSComputerName","CN","ServerName")]
 		[Parameter(
 			Position=0,
 			ValueFromPipeline,
@@ -90,14 +103,12 @@ function Get-ComputerInventory {
 		[Alias("Destination","DestinationPath")]
 		[ValidateScript(
 			# Validate the Path specified by the user
-			{Test-Path -path $_})]
-		[String]$OutputPath,
+			{Test-Path -path $_ })]
+		[String]$Path,
 	
-		[Parameter(
-			Mandatory,
-			HelpMessage="Specify the protocol to use")]
+		[Parameter()]
 		[ValidateSet("WSMAN","DCOM")]
-		[String]$Protocol = 'WSMAN',
+		[String]$Protocol,
 		
 		[Switch]$HardwareInformation,
 		
@@ -105,7 +116,7 @@ function Get-ComputerInventory {
 	
 		[Switch]$LastReboot,
 	
-		[Switch]$ApplicationInstalled,
+		[Switch]$ApplicationsInstalled,
 	
 		[Switch]$WindowsComponents
 	)
@@ -114,9 +125,10 @@ function Get-ComputerInventory {
 		TRY {
 			# Verify CimCmdlets is loaded (CIM is loaded by default)
 			#IF(-not(Get-Module -Name CimCmdlets -ErrorAction Stop | Out-Null){Import-Module -Name CimCmdlets}
-		}#TRY
+		}#TRY Block
 		CATCH {
 		}#CATCH Block
+		
 	}#BEGIN Block
 	
 	
@@ -128,28 +140,19 @@ function Get-ComputerInventory {
 				ComputerName 	= $Computer
 				ErrorAction 	= 'Stop'
 				ErrorVariable	= 'ProcessErrorCIM'
-			}#$CIMSessionParams
-			
+			}
 			
 			TRY {
 				
-				# Connectivity Test
+				# Connectivity
                 Write-Verbose -Message "$Computer - Testing Connection..."
                 Test-Connection -ComputerName $Computer -count 1 -Quiet -ErrorAction Stop -ErrorVariable ProcessErrorTestConnection | Out-Null
-				
+								
 				# Credential
 				IF ($PSBoundParameters['Credential']) {$CIMSessionParams.credential = $Credential}
 				
-				# Protocol
-				IF ($PSBoundParameters['Protocol'] = 'DCOM'){
-					# Trying with DCOM protocol
-            		Write-Verbose -Message "$Computer - Trying to connect via DCOM protocol"
-            		$CIMSessionParams.SessionOption = New-CimSessionOption -Protocol Dcom
-            		$CimSession = New-CimSession @CIMSessionParams
-            		$CimProtocol = $CimSession.protocol
-            		Write-Verbose -message "$Computer - [$CimProtocol] CIM SESSION - Opened"
-				}# IF 'DCOM'
-				ELSE{
+				# Protocol not specified
+				IF (-not($PSBoundParameters['Protocol'])){
 					# Trying with WsMan protocol
 					Write-Verbose -Message "$Computer - Trying to connect via WSMAN protocol"
 					IF ((Test-WSMan -ComputerName $Computer -ErrorAction SilentlyContinue).productversion -match 'Stack: 3.0'){
@@ -158,69 +161,142 @@ function Get-ComputerInventory {
             			$CimProtocol = $CimSession.protocol
             			Write-Verbose -message "$Computer - [$CimProtocol] CIM SESSION - Opened"
 					}#IF
-				}# ELSE 'WSMAN'
+					ELSE{
+						# Trying with DCOM protocol
+						Write-Verbose -message "$Computer - WSMAN protocol does not work, failing back to DCOM"
+            			Write-Verbose -Message "$Computer - Trying to connect via DCOM protocol"
+	            		$CIMSessionParams.SessionOption = New-CimSessionOption -Protocol Dcom
+	            		$CimSession = New-CimSession @CIMSessionParams
+	            		$CimProtocol = $CimSession.protocol
+	            		Write-Verbose -message "$Computer - [$CimProtocol] CIM SESSION - Opened"
+					}#ELSE
+				}#IF Block
 				
-				# Data
 				
-				$Info = @{
+				# Protocol Specified
+				IF ($PSBoundParameters['Protocol']){
+					SWITCH ($protocol) {
+						"WSMAN" {
+							Write-Verbose -Message "$Computer - Trying to connect via WSMAN protocol"
+							IF ((Test-WSMan -ComputerName $Computer -ErrorAction Stop -ErrorVariable ProcessErrorTestWsMan).productversion -match 'Stack: 3.0') {
+								Write-Verbose -Message "$Computer - WSMAN is responsive"
+		            			$CimSession = New-CimSession @CIMSessionParams
+		            			$CimProtocol = $CimSession.protocol
+		            			Write-Verbose -message "$Computer - [$CimProtocol] CIM SESSION - Opened"
+							}
+						}
+						"DCOM" {
+							Write-Verbose -Message "$Computer - Trying to connect via DCOM protocol"
+		            		$CIMSessionParams.SessionOption = New-CimSessionOption -Protocol Dcom
+		            		$CimSession = New-CimSession @CIMSessionParams
+		            		$CimProtocol = $CimSession.protocol
+		            		Write-Verbose -message "$Computer - [$CimProtocol] CIM SESSION - Opened"
+						}
+					}
+				}
+				
+				# Prepare Output Variable
+				$Inventory = @{
 					ComputerName = $Computer
-				}#$Info
+					Connectivity = 'Online'
+				}
+		
+				# HardwareInformation Switch Parameter
+				IF ($HardwareInformation) {
+					Write-Verbose -Message "$Computer - Gather Hardware Information"	
 				
-				IF ($PSBoundParameters['HardwareInformation']) {
 					# Get the information from Win32_ComputerSystem
-					$ComputerSystem = Get-CimInstance -CimSession $CimSession -ClassName win32_ComputerSystem -Property Manufacturer,Model,TotalPhysicalMemory,NumberOfProcessors
+					$ComputerSystem = Get-CimInstance -CimSession $CimSession -ClassName win32_ComputerSystem #-Property Manufacturer,Model,TotalPhysicalMemory,NumberOfProcessors
 					
 					# Get the information from Win32_diskdrive
-					$DiskDrive = Get-CimInstance -CimSession $CimSession -ClassName win32_diskdrive -Property Size
+					$DiskDrive = Get-CimInstance -CimSession $CimSession -ClassName win32_diskdrive # -Property Size
 					
-					# Send the Info to the array
-					$Info.Manufacturer = $ComputerSystem.Manufacturer
-					$Info.Model = $ComputerSystem.Model
-					$Info.MemoryGB = $ComputerSystem.TotalPhysicalMemory/1GB
-					$Info.NumberOfProcessors = $ComputerName.NumberOfProcessors
-					$Info.DiskSize = $DiskDrive.Size/1GB
-					
-					}
+					# Send the Information to the $Inventory object
+					$Inventory.Manufacturer = $ComputerSystem.Manufacturer
+					$Inventory.Model = $ComputerSystem.Model
+					$Inventory.MemoryGB = "{0:N2}" -f ($ComputerSystem.TotalPhysicalMemory/1GB)
+					$Inventory.NumberOfProcessors = $ComputerSystem.NumberOfProcessors
+					$Inventory.LocalDisks = $DiskDrive | Select-Object -Property DeviceID,@{Label="SizeGB";Expression={"{0:N2}" -f ($_.Size/1GB)}},SerialNumber,Model,Manufacturer,InterfaceType
+												
+				}#IF ($HardwareInformation)
 				
-				# Switch LastPatchInstalled
+				
+				# LastPatchInstalled Switch Parameter
 				IF ($LastPatchInstalled) {
+					Write-Verbose -Message "$Computer - Gather Last Patch Installed"
 					
 					# Get the information from win32_quickfixengineering
-					$LastPatchesInstalled = get-ciminstance -CimSession $CimSession -ClassName win32_quickfixengineering -Property Installedon
+					$LastPatchesInstalled = Get-CimInstance -CimSession $CimSession -ClassName win32_quickfixengineering #-Property Installedon
 					
-					# Send the information to the array
-					$Info.LastPatchInstalled = ($LastPatchesInstalled | Sort-Object -Property InstalledOn -Descending | Select-Object -first 1).HotFixID
-					}
+					# Send the Information to the $Inventory object
+					$Inventory.LastPatchInstalled = $LastPatchesInstalled | Sort-Object -Property InstalledOn -Descending | Select-Object -Property HotFixID,Caption,Description -first 1
+				}#IF ($LastPatchInstalled)
 				
-				# Switch LastReboot
+				
+				# LastReboot Switch Parameter
 				IF ($LastReboot) {
-					# Get the information from Win32_OperatingSystem
-					$LastReboot = get-ciminstance -CimSession $CimSession -ClassName win32_operatingsystem -Property LastBootUpTime
+					Write-Verbose -Message "$Computer - Gather Last Reboot DateTime"
 					
-					# Send the information to the array
-					$Info.LastReboot = $LastReboot.LastBootUpTime
-				}
-				
-				# Switch ApplicationInstalled
-				IF ($ApplicationInstalled) {
 					# Get the information from Win32_OperatingSystem
-					$Services = get-ciminstance -CimSession $CimSession -ClassName win32_service -Property Name,State,Status
-					
+					$OperatingSystem = Get-CimInstance -CimSession $CimSession -ClassName win32_operatingsystem -Property LastBootUpTime
 					# Send the information to the array
-					$Info.SQLInstalled = 
-				}
+					$Inventory.LastReboot = $OperatingSystem.LastBootUpTime
+				}#IF ($LastReboot)
 				
-				# Switch WindowsComponents
+				
+				# ApplicationInstalled Switch Parameter
+				IF ($ApplicationsInstalled) {
+					Write-Verbose -Message "$Computer - Gather Application Installed"
+					
+					# Get the information from Win32_OperatingSystem
+					$Services = Get-CimInstance -CimSession $CimSession -ClassName win32_service
+					
+					# Send the Information to the $Inventory object for each application
+					IF ($Services | Where-Object {$_.name -like 'sqlserver*'}){$Inventory.SQLInstalled = $true} # SQL Service Check
+					IF ($Services | Where-Object {$_.name -like 'w3scv*'}){$Inventory.IISInstalled = $true} # IIS Service Check
+					IF ($Services | Where-Object {$_.name -like '*sharepoint*'}){$Inventory.SharepointInstalled = $true}# Sharepoint Service Check
+					IF ($Services | Where-Object {$_.name -like '*msexchange*'}){$Inventory.ExchangeInstalled = $true}# Exchange Service Check
+				}#IF ($ApplicationInstalled)
+				
+				
+				# WindowsComponents Switch Parameter
 				IF ($WindowsComponents) {
+					Write-Verbose -Message "$Computer - Gather Windows Components Installed"
 					
-				}
+					# Get the information from Win32_OptionalFeature
+					$WindowsFeatures = Get-CimInstance -CimSession $CimSession -ClassName Win32_OptionalFeature #-Property Caption
+					
+					# Send the Information to the $Inventory object
+					$Inventory.WindowsComponents = $WindowsFeatures | Select-Object -Property Name,Caption
+				}#IF ($WindowsComponents)
+				
+				
+				# Output to the console
+				Write-Verbose -Message "$Computer - Output information"
+				[pscustomobject]$Inventory
+				
+				
+				# Output to a XML file
+				IF ($PSBoundParameters['Path']) {
+					$DateFormat = Get-Date -Format 'yyyyMMdd_HHmmss'
+					$FileFormat = "Inventory-$Computer-$DateFormat.xml"
+					Write-Verbose -Message "$Computer - Output Data to a XML file: $fileformat"
+					[pscustomobject]$Inventory | Export-Clixml -Path (Join-Path -Path $Path -ChildPath $FileFormat) -ErrorAction 'Stop' -ErrorVariable ProcessErrorExportCLIXML		
+				}#IF ($PSBoundParameters['Path'])
 				
 			}#TRY Block
+			
 			CATCH {
 				IF ($ProcessErrorTestConnection){Write-Warning -Message "$Computer - Can't Reach"}
 				IF ($ProcessErrorCIM){Write-Warning -Message "$Computer - Can't Connect - $protocol"}
-				
+				IF ($ProcessErrorTestWsMan){Write-Warning -Message "$Computer - Can't Connect - $protocol"}
+				IF ($ProcessErrorExportCLIXML){Write-Warning -Message "$Computer - Can't Export the XML file $fileformat in $Path"}
 			}#CATCH Block
+			
+			FINALLY{
+				Write-Verbose "Removing CIM 3.0 Session from $Computer"
+                IF ($CimSession) {Remove-CimSession $CimSession}
+			}#FINALLY Block
 		}#FOREACH Block
 	}#PROCESS Block
 	
@@ -232,7 +308,7 @@ function Get-ComputerInventory {
 		CATCH {
 		}#CATCH Block
 	}#END Block
-}#Function Get-ComputerInformation
+}#Function Get-ComputerInventory
 
 
 
