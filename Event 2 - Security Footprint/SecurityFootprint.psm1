58c283bef7ebe1e49e2ae17df7cc3eb2b9be936d
DexterPOSH <dexter34925@yahoo.in>
2014-02-01 18:11:04 +0530

    Update SecurityFootprint.psm1
    Ste-SecurityMeasure -- specify a path to the folder containing sensitive information and it will set restrictive ACL on it (also an option to encrypt it)

--- Event 2 - Security Footprint/SecurityFootprint.psm1
+++ Event 2 - Security Footprint/SecurityFootprint.psm1
@@ -46,3 +46,146 @@ $differenceObject = "C:\Users\gulicst1\SkyDrive\Scripting\Githhub\WinterScriptin
 Get-XMlDifferences -ReferenceObject $ReferenceObject -differenceObject $differenceObject
 #Not finished yet
 #EndRegion
+
+
+#region Set-SecurityMeasure
+
+function Set-SecurityMeasure
+{
+<#
+.Synopsis
+   Function Takes a Folder path and sets the restricted Folder permissions on it and gives an option to encrypt it
+.DESCRIPTION
+   This Function will take a path to a folder and block inheritance of ACL on it and then will set the Current User
+    running the Function as the owner of that Folder. If the Script detects already the appropraite ACLs are set then
+    no changes are made. It gives an option to Encrypt the folder.
+.PARAMETER Path
+    Specify the path of the folder where restrictive permissions are to be set.
+.PARAMETER Encrypt
+    Specify this Swicth if you want to encrypt the Input Path Folder as well.
+.PARAMETER Force
+    Specify this Switch if you want to force the changes.
+.PARAMETER PassThru
+    Specify this switch to get the ACL Object back at the end.
+.EXAMPLE
+    Following example first create a Folder where sensitive data is stored.
+    Then put the restrictive ACLs on the Folder to allow only the Script owner the access.
+    PS> Mkdir Dexter1
+    PS> Set-SecurityMeasure -Path .\Dexter1 -Verbose
+.EXAMPLE
+     One can pipe the String to the Folder path to the Function as well. 
+     Specify Switch Passthru to get the new ACL back  on the folder.
+
+     PS> "C:\Temp\Test1" | Set-SecurityMeasure -Verbose  -PassThru
+        VERBOSE: [BEGIN] Starting the Function Set-SecurityMeasure
+        VERBOSE: [PROCESS]
+        VERBOSE: [PROCESS] Settting the Administrator as the Owner for the .\test1
+        VERBOSE: [PROCESS] Setting the modified ACL to the .\test1
+        VERBOSE: Performing the operation "Set-Acl" on target "C:\temp\test1".
+
+
+            Directory: C:\temp
+
+
+        Path                                                   Owner                                                  Access                                                
+        ----                                                   -----                                                  ------                                                
+        test1                                                  DEXTER\Administrator                                   DEXTER\Administrator Allow  FullControl               
+        VERBOSE: [END] Ending the Function Set-SecurityMeasure
+.INPUTS
+   [System.String]
+.OUTPUTS
+   System.Security.AccessControl.DirectorySecurity
+
+#>   
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                  ConfirmImpact='High')]
+    [OutputType([String])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory, 
+                   ValueFromPipeline,
+                   ValueFromPipelineByPropertyName)]
+        [ValidateScript({Test-Path -Path $_ -PathType Container })]
+        [Alias("Fullname")]
+        [string]$Path,
+
+        [switch]$Encrypt,
+
+        [switch]$Force,
+
+        [switch]$PassThru
+    )
+
+    BEGIN
+    {
+        Write-Verbose -Message "[BEGIN] Starting the Function Set-SecurityMeasure"
+        $PSBoundParameters.Remove('Force') | Out-Null
+        $PSBoundParameters.Remove('Encrypt') | Out-Null
+        $PSBoundParameters.Confirm = $False
+        
+    }
+    PROCESS
+    {
+        Write-Verbose -Message "[PROCESS]"
+        if ( $Force -or $pscmdlet.ShouldProcess("$Path", "Setting Restricted ACL"))
+        {
+            #Get-Acl - Store the ACL 
+            $acl = Get-Acl -Path $Path
+
+            #Remove Inherited Rules and protect the rules from being changed by inheritance
+            $acl.SetAccessRuleProtection($True, $False)
+            #Purge all the exisiting ACEs inside the ACL 
+            $acl.Access | foreach {$acl.PurgeAccessRules($_.IdentityReference)}
+
+            #set the User running the Script as the owner
+            Write-Verbose -Message "[PROCESS] Settting the $env:Username as the Owner for the $Path"
+            $acl.SetOwner([System.Security.Principal.NTAccount]"$env:username")
+
+            #Give the Owner the Full access on the Folder
+            $dirAce = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList "$env:Username","FullControl", "ContainerInherit, ObjectInherit", "None", "Allow"
+            $acl.AddAccessRule($dirAce)
+            
+            #now compare the Access on the Path to the Access we are giving...if there is a change then only set it
+            if ((Compare-Object -ReferenceObject (Get-Acl -Path $Path).access  -DifferenceObject $acl.access) -or ($acl.Owner -ne (Get-acl $Path).owner ))
+            {
+                #set the ACL Now as it seems to be modified
+                Write-Verbose -Message "[PROCESS] Setting the modified ACL to the $Path"
+                Set-Acl @PSBoundParameters -AclObject $acl 
+            }#END IF
+
+            if ($Encrypt)
+            {
+                Write-Verbose -Message "[PROCESS] Checkig if the folder $Path is encrypted now"
+                if ((Get-ItemProperty -Path $Path).attributes -match "Encrypted")
+                {
+                    Write-Verbose -Message "[PROCESS] The Folder $Path is already encrypted"
+                } #End IF
+                else
+                {
+                   try
+                   { 
+                        Write-Verbose -Message "[PROCESS] Trying to Encrypt the $Path"
+                        #Set ErrorActionPreference to Stop to catch exceptions from cipher.exe
+                        $ErrorActionPreference = 'stop'
+                        & cipher.exe /E $Path  
+                        Write-Verbose -Message "[PROCESS] The folder $Path has been encrypted"
+                   }
+                   catch
+                   {
+                        Write-Warning -Message "Something went wrong while trying to encrypt $path"
+                        Write-Error -message  $($_.exception )
+                   }#End Catch
+                }#end Else
+            }#End IF
+
+        }#End IF
+        
+    }#End PROCESS
+    END
+    {
+        Write-Verbose -Message "[END] Ending the Function Set-SecurityMeasure"
+    }
+}
+
+#endregion Set-SecurityMeasure
